6f2ab5b7104f8b8c1f0dccaa5ecbc6b071fb8c08
Alexandr Kravchenko <kravchenkoalexandr19@gmail.com>
2021-05-25 00:23:17 +0300

    wip

---
+++ app/components/modules/chords/Search/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  height: 100%;
+`;
+
+export default Container;
--- app/components/modules/chords/View/Container.js
+++ app/components/modules/chords/View/Container.js
@@ -4,6 +4,7 @@ const Container = styled.div`
   height: 100%;
   display: grid;
   place-items: center;
+  background-color: #FFFFFF;
 `;
 
 export default Container;
